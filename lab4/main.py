039c649fa5d151bea1182690a3b8389060cc96f2
Makar Solomatin <solomatin.makar.ms5@gmail.com>
2021-01-25 21:08:57 +0300

    add u3(u2)

--- lab4/main.py
+++ lab4/main.py
@@ -1,8 +1,9 @@
 import numpy as np
 import scipy.optimize as opt
+import matplotlib.pyplot as plt
 from tolsolvty import tolsolvty
 
-A = np.loadtxt("A.txt")
+A = np.loadtxt("a.txt")
 inf_b = np.loadtxt("b_inf.txt").reshape(3, 1)
 sup_b = np.loadtxt("b_sup.txt").reshape(3, 1)
 [tolmax, argmax, envs, ccode] = tolsolvty(A, A, inf_b, sup_b)
@@ -24,25 +25,25 @@ pos_sign = (0, +np.inf)
 any_sign = (-np.inf, +np.inf)
 str_sign = (0, -tolmax[0])
 
-res = opt.linprog([0, 0, 0, 1, 1, 1],
-                      A_ub=C,
-                      b_ub=r,
-                      bounds=[any_sign, any_sign, any_sign,
-                              pos_sign, pos_sign, pos_sign],
-                      method='interior-point')
-print('res = ', res.x)
-print('status: ', res.status)
 
-#inf_b -= np.asarray([-tolmax[0]] * 3).reshape(3, 1)#res.x[-3:].reshape(3, 1)
-#sup_b += np.asarray([-tolmax[0]] * 3).reshape(3, 1)#res.x[-3:].reshape(3, 1)
-#[tolmax, argmax, _, __] = tolsolvty(A, A, inf_b, sup_b)
-#
-#print("max Tol = ", tolmax, "argmax = ", argmax)
-#
-#print(np.matmul(A, argmax) - inf_b, sup_b - np.matmul(A, argmax))
-#
-#xx = np.concatenate((argmax,
-#                     np.asarray(
-#                         [-oldtolmax / diag_rad_b[0, 0], -oldtolmax / diag_rad_b[1, 1], -oldtolmax / diag_rad_b[2, 2]]).
-#                     reshape(3, 1)), axis=0)
-#print("Cx - r = ", np.matmul(C, xx) - r)
+x = []
+y = []
+for i in range(10):
+    res = opt.linprog([0, 0, 0, 1, 1, 1],
+                          A_ub=C,
+                          b_ub=r,
+                          bounds=[any_sign, (i,i+1), any_sign,
+                                  pos_sign, pos_sign, pos_sign],
+                          method='interior-point')
+    u = res.x
+    x.append(u[1])
+    y.append(u[2])
+
+print(f"x: {x}")
+print(f"y: {y}")
+plt.plot(x, y)
+plt.xlabel('u2')
+plt.ylabel('u3')
+plt.title('u3(u2)')
+plt.grid()
+plt.show()
